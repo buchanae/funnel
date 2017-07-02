fd9dfd8484df8b7e370ad4ce054ad5fa73ea917f
Alex Buchanan <buchanae@ohsu.edu>
2017-07-02 09:02:25 -0700

    WIP

---
+++ gcp/storage.go
@@ -0,0 +1,209 @@
+package gcp
+
+import (
+	"context"
+	"fmt"
+	"github.com/ohsu-comp-bio/funnel/proto/tes"
+	"github.com/ohsu-comp-bio/funnel/util"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/storage/v1"
+	"io"
+	"io/ioutil"
+	"net/http"
+	urllib "net/url"
+	"os"
+	"path"
+	"strings"
+)
+
+// The gs url protocol
+const gsscheme = "gs"
+
+type Storage struct {
+	svc *storage.Service
+}
+
+func NewStorage(conf StorageConfig) (*Storage, error) {
+	ctx := context.Background()
+	client := &http.Client{}
+
+	if conf.AccountFile != "" {
+		// Pull the client configuration (e.g. auth) from a given account file.
+		// This is likely downloaded from Google Cloud manually via IAM & Admin > Service accounts.
+		bytes, rerr := ioutil.ReadFile(conf.AccountFile)
+		if rerr != nil {
+			return nil, rerr
+		}
+
+		config, tserr := google.JWTConfigFromJSON(bytes, storage.CloudPlatformScope)
+		if tserr != nil {
+			return nil, tserr
+		}
+		client = config.Client(ctx)
+	} else if conf.FromEnv {
+		// Pull the information (auth and other config) from the environment,
+		// which is useful when this code is running in a Google Compute instance.
+		defClient, err := google.DefaultClient(ctx, storage.CloudPlatformScope)
+		if err == nil {
+			client = defClient
+		} else {
+			log.Error("Error connecting Google Storage client. Defaulting to anonymous.", err)
+			// No auth config could be found, so default to anonymous.
+		}
+	}
+
+	svc, cerr := storage.New(client)
+	if cerr != nil {
+		return nil, cerr
+	}
+
+	return &Storage{svc}, nil
+}
+
+// Get copies an object from GS to the host path.
+func (gs *Storage) Get(ctx context.Context, rawurl string, hostPath string, class tes.FileType) error {
+	log.Info("Starting download", "url", rawurl)
+
+	url, perr := parse(rawurl)
+	if perr != nil {
+		return perr
+	}
+
+	if class == tes.FileType_FILE {
+		call := gs.svc.Objects.Get(url.bucket, url.path)
+		err := download(call, hostPath)
+		if err != nil {
+			return err
+		}
+		log.Info("Finished file download", "url", rawurl, "hostPath", hostPath)
+		return nil
+
+	} else if class == tes.FileType_DIRECTORY {
+		// TODO not handling pagination
+		objects, _ := gs.svc.Objects.List(url.bucket).Prefix(url.path).Do()
+		for _, obj := range objects.Items {
+			call := gs.svc.Objects.Get(url.bucket, obj.Name)
+			err := download(call, path.Join(hostPath, obj.Name))
+			if err != nil {
+				return err
+			}
+		}
+		log.Info("Finished directory download", "url", rawurl, "hostPath", hostPath)
+		return nil
+	}
+	return fmt.Errorf("Unknown file class: %s", class)
+}
+
+func download(call *storage.ObjectsGetCall, hostPath string) error {
+	resp, derr := call.Download()
+	if derr != nil {
+		return derr
+	}
+
+	util.EnsurePath(hostPath)
+	dest, cerr := os.Create(hostPath)
+	if cerr != nil {
+		return cerr
+	}
+
+	_, werr := io.Copy(dest, resp.Body)
+	if werr != nil {
+		return werr
+	}
+	return nil
+}
+
+// Put copies an object (file) from the host path to GS.
+func (gs *Storage) Put(ctx context.Context, rawurl string, hostPath string, class tes.FileType) ([]*tes.OutputFileLog, error) {
+	log.Info("Starting upload", "url", rawurl)
+
+	var out []*tes.OutputFileLog
+
+	switch class {
+	case File:
+		err := gs.put(ctx, rawurl, hostPath)
+		if err != nil {
+			return nil, err
+		}
+		out = append(out, &tes.OutputFileLog{
+			Url:       rawurl,
+			Path:      hostPath,
+			SizeBytes: fileSize(hostPath),
+		})
+
+	case Directory:
+		files, err := walkFiles(hostPath)
+
+		for _, f := range files {
+			u := rawurl + "/" + f.rel
+			out = append(out, &tes.OutputFileLog{
+				Url:       u,
+				Path:      f.abs,
+				SizeBytes: f.size,
+			})
+			err := gs.put(ctx, u, f.abs)
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+	default:
+		return nil, fmt.Errorf("Unknown file class: %s", class)
+	}
+
+	log.Info("Finished upload", "url", rawurl, "hostPath", hostPath)
+	return out, nil
+}
+
+func (gs *Storage) put(ctx context.Context, rawurl, hostPath string) error {
+
+	url, perr := parse(rawurl)
+	if perr != nil {
+		return perr
+	}
+
+	reader, oerr := os.Open(hostPath)
+	if oerr != nil {
+		return oerr
+	}
+
+	obj := &storage.Object{
+		Name: url.path,
+	}
+
+	_, err := gs.svc.Objects.Insert(url.bucket, obj).Media(reader).Do()
+	return err
+}
+
+// Supports returns true if this backend supports the given storage request.
+// The Google Storage backend supports URLs which have a "gs://" scheme.
+func (gs *Storage) Supports(rawurl string, hostPath string, class tes.FileType) bool {
+	_, err := parse(rawurl)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
+type urlparts struct {
+	bucket string
+	path   string
+}
+
+func parse(rawurl string) (*urlparts, error) {
+	url, err := urllib.Parse(rawurl)
+	if err != nil {
+		return nil, err
+	}
+	if url.Scheme != gsscheme {
+		return nil, fmt.Errorf("Invalid URL scheme '%s' for Google Storage backend in url: %s", url.Scheme, rawurl)
+	}
+
+	bucket := url.Host
+	path := strings.TrimLeft(url.EscapedPath(), "/")
+	return &urlparts{bucket, path}, nil
+}
--- gcp/tes.go
+++ gcp/tes.go
@@ -1,6 +1,8 @@
 package gcp
 
 import (
+  "bytes"
+  "sync"
 	"github.com/ohsu-comp-bio/funnel/logger"
 	uuid "github.com/nu7hatch/gouuid"
 	"github.com/ohsu-comp-bio/funnel/proto/tes"
@@ -8,6 +10,7 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/metadata"
+  "cloud.google.com/go/storage"
 )
 
 var log = logger.Sub("gcp")
@@ -20,35 +23,163 @@ func NewGCP(conf Config) error {
   ctx := context.Background()
 }
 
+var startupScript = `
+#!/bin/sh
+docker run          \
+  --group-add 412   \
+  --name funnel     \
+  -w /var/db/funnel \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /usr/bin/docker:/usr/bin/docker           \
+  -v /var/db/funnel:/var/db/funnel             \
+  docker.io/ohsucompbio/funnel:latest gce start
+
+shutdown
+`
+
+type partial struct {
+  task *tes.Task
+  handle *storage.ObjectHandle
+}
+
+type partials []partial
+func (ps partials) read(ctx) error {
+}
+func (ps partials) write(ctx) error {
+  ctx, cancel = context.WithCancel(ctx)
+  defer cancel()
+
+  errs := make(chan error)
+  for _, p := range ps {
+    go func(p partial) {
+      w := p.handle.NewWriter(ctx)
+      errs <- marshaler.Marshal(w, p.task)
+      errs <- w.Close()
+    }(p)
+  }
+
+  for i := 0; i < len(ps) * 2; i++ {
+    if err := <-errs; err != nil {
+      return error
+    }
+  }
+
+  return nil
+}
+
+
+
+type taskHandle struct {
+  bkt string
+  id string
+}
+func (h *taskHandle) URL() string {
+  return fmt.Sprintf("tasks/%s/", h.id)
+}
+func (h *taskHandle) Base() *storage.ObjectHandle {
+  p := fmt.Sprintf("tasks/%s/task.base.json", h.id)
+  return h.bkt.Object(p)
+}
+func (h *taskHandle) State() *storage.ObjectHandle {
+  p := fmt.Sprintf("tasks/%s/task.state.json", h.id)
+  return h.bkt.Object(p)
+}
+func (h *taskHandle) Stdout() *storage.ObjectHandle {
+  p := fmt.Sprintf("tasks/%s/task.stdout.json", h.id)
+  return h.bkt.Object(p)
+}
+func (h *taskHandle) Stderr() *storage.ObjectHandle {
+  p := fmt.Sprintf("tasks/%s/task.stderr.json", h.id)
+  return h.bkt.Object(p)
+}
+func (h *taskHandle) Contents() *storage.ObjectHandle {
+  p := fmt.Sprintf("tasks/%s/task.contents.json", h.id)
+  return h.bkt.Object(p)
+}
+
+
 
 func (gcp *GCP) CreateTask(ctx context.Context, task *tes.Task) (*tes.CreateTaskResponse, error) {
+  ctx, cancel = context.WithCancel(ctx)
+  defer cancel()
 
-  // TODO use datastore ID generation? then prefix with time and hash?
 	task.Id := util.GenTaskID()
-
-  project := ""
+  project := "funnel-165618"
   zone := "us-west1-a"
+  machineType := "n1-standard-1"
+  diskSize := int64(50)
 
-	// Prepare disk details by setting the specific zone
-	for _, disk := range props.Disks {
-		dt := localize(zone, "diskTypes", disk.InitializeParams.DiskType)
-		disk.InitializeParams.DiskType = dt
-	}
+  h := taskHandle{store, "funnel", task.Id}
+  // Split task input contents into a separate task.
+  // This modifies "task"
+  contents := splitInputContents(task)
+  taskURL := h.URL()
 
-	// Create the instance on GCE
+  // Write the task message as JSON
+  p := []partials{
+    // Base task message
+    {
+      task: task,
+      handle: h.Base(),
+    },
+    // Task state
+    {
+      task: &tes.Task{State: tes.State_INITIALIZING},
+      handle: h.State(),
+    },
+    // Task input contents
+    {
+      task: contents,
+      handle: h.Contents(),
+    },
+    // Task stdout
+    {
+      task: &tes.Task{},
+      handle: h.Stdout(),
+    },
+    // Task stderr
+    {
+      task: &tes.Task{},
+      handle: h.Stderr(),
+    },
+  }
+
+  // Create a worker
 	instance := compute.Instance{
 		Name:              genWorkerID(),
 		Description:       fmt.Sprintf("Funnel worker for task ID %s", task.Id),
-		Disks:             props.Disks,
-		MachineType:       localize(zone, "machineTypes", props.MachineType),
-		Tags:              props.Tags,
-		Metadata:          &metadata,
+		Disks: []*compute.AttachedDisk{
+      {
+        AutoDelete: true,
+        Boot: true,
+        InitializeParams: &compute.AttachedDiskInitializeParams{
+          DiskSizeGb: diskSize,
+        },
+      },
+    },
+		MachineType:       localize(zone, "machineTypes", machineType,
+    Metadata: &compute.Metadata{
+      Items: []*compute.MetadataItems{
+        {
+          Key: "taskID",
+          Value: &task.Id,
+        },
+        {
+          Key: "taskURL",
+          Value: &taskURL,
+        },
+        {
+          Key: "startup-script",
+          Value: &startupScript,
+        },
+      },
+    },
 	}
 
-	op, ierr := s.gce.InsertInstance(project, zone, &instance)
+	op, ierr := comp.InsertInstance(project, zone, &instance)
 	if ierr != nil {
 		log.Error("Couldn't insert GCE VM instance", ierr)
-		return ierr
+		return nil, ierr
 	}
 	log.Debug("GCE VM instance created", "details", op)
 
@@ -60,28 +191,75 @@ func (gcp *GCP) CreateTask(ctx context.Context, task *tes.Task) (*tes.CreateTask
 
 
 func (gcp *GCP) GetTask(ctx context.Context, req *tes.GetTaskRequest) (*tes.Task, error) {
+  ctx, cancel := context.WithCancel(ctx)
+  defer cancel()
 
-  task := tes.Task{}
-  var keys []*datastore.Key
-  var ents []*tes.Task
+  bucketName := "funnel"
+  project := "funnel-165618"
+  zone := "us-west1-a"
+  base := "/tasks/" + task.Id + "/"
+  task := &tes.Task{}
+
+  bkt := store.Bucket(bucketName)
 
   switch req.View {
   case tes.TaskView_FULL:
+    ps = append(ps, partial{
+      task: task,
+      handle: h.Stdout(),
+    })
+    ps = append(ps, partial{
+      task: task,
+      handle: h.Stderr(),
+    })
+    ps = append(ps, partial{
+      task: task,
+      handle: h.Contents(),
+    })
     fallthrough
 
   case tes.TaskView_BASIC:
+    ps = append(ps, partial{
+      task: task,
+      handle: h.Base(),
+    })
     fallthrough
 
   default:
+    ps = append(ps, partial{
+      task: task,
+      handle: h.State(),
+    })
   }
 
-	return task, err
+  if err := ps.read(ctx); err != nil {
+    return nil, err
+  }
+
+	return task, nil
 }
 
 
 
 func (gcp *GCP) ListTasks(ctx context.Context, req *tes.ListTasksRequest) (*tes.ListTasksResponse, error) {
+
+  bucket := "funnel"
+  project := "funnel-165618"
+  zone := "us-west1-a"
   resp := tes.ListTasksResponse{}
+
+  objects, _ := objs.List(bucket).Prefix("tasks").Do()
+  for _, obj := range objects.Items {
+    call := objs.Get(url.bucket, obj.Name)
+
+    // TODO get tasks in parallel
+    task, err := gcp.GetTask(ctx, &tes.GetTaskRequest{id, req.View})
+
+    if err != nil {
+      return err
+    }
+    resp.Tasks = append(resp.Tasks, task)
+  }
 
 	return &resp, nil
 }
@@ -90,6 +268,17 @@ func (gcp *GCP) ListTasks(ctx context.Context, req *tes.ListTasksRequest) (*tes.
 
 // CancelTask cancels a task
 func (gcp *GCP) CancelTask(ctx context.Context, req *tes.CancelTaskRequest) (*tes.CancelTaskResponse, error) {
+
+  bucket := "funnel"
+  project := "funnel-165618"
+  zone := "us-west1-a"
+
+  st := strings.NewReader(tes.State_CANCELED.String())
+
+	_, err := objs.Insert(bucket, &storage.Object{
+		Name: "tasks/" + req.Id + "/task.state",
+  }).Media(st).Do()
+
 	return &tes.CancelTaskResponse{}, nil
 }
 
