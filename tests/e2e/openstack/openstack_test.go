321164c7158e950a67e54a464c825520ecfd250d
Alex Buchanan <buchanae@ohsu.edu>
2017-09-12 08:51:15 -0700

    Add swift directory test

--- tests/e2e/openstack/openstack_test.go
+++ tests/e2e/openstack/openstack_test.go
@@ -74,3 +74,39 @@ func TestSwiftStorageTask(t *testing.T) {
 		t.Fatal("unexpected content", string(b))
 	}
 }
+
+func TestSwiftDirStorageTask(t *testing.T) {
+	id := fun.Run(`
+    --cmd "sh -c 'mkdir -p $out/dir; md5sum $in > $out/dir/md5.txt'"
+    -i in=swift://buchanan-scratch/funnel
+    -O out=swift://buchanan-scratch/funnel-md5-dir
+  `)
+	task := fun.Wait(id)
+
+	expect := "da385a552397a4ac86ee6444a8f9ae3e  /opt/funnel/inputs/buchanan-scratch/funnel\n"
+
+	if task.State != tes.State_COMPLETE {
+		t.Fatal("Unexpected task failure")
+	}
+
+	s := storage.Storage{}
+	s, serr := s.WithConfig(conf.Worker.Storage)
+	if serr != nil {
+		t.Fatal("Error configuring storage", serr)
+	}
+
+	ctx := context.Background()
+	gerr := s.Get(ctx, "swift://buchanan-scratch/funnel-md5-dir", "swift-md5-out-dir", tes.FileType_DIRECTORY)
+	if gerr != nil {
+		t.Fatal("Failed get", gerr.Error())
+	}
+
+	b, err := ioutil.ReadFile("swift-md5-out-dir/dir/md5.txt")
+	if err != nil {
+		t.Fatal("Failed read", err)
+	}
+
+	if string(b) != expect {
+		t.Fatal("unexpected content", string(b))
+	}
+}
