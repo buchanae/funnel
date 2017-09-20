be5b6e1a36171e947a26ca1e524d5b470f077569
Alex Buchanan <buchanae@ohsu.edu>
2017-09-20 08:21:59 -0700

    WIP

--- proto/tes/utils.go
+++ proto/tes/utils.go
@@ -10,3 +10,34 @@ func TerminalState(s State) bool {
 	return s == State_COMPLETE || s == State_ERROR || s == State_SYSTEM_ERROR ||
 		s == State_CANCELED
 }
+
+func (t *tes.Task) GetTaskLog(i int) *tes.TaskLog {
+
+	// Grow slice length if necessary
+	if len(t.Logs) <= i {
+		desired := i + 1
+		t.Logs = append(t.Logs, make([]*TaskLog, desired-len(t.Logs))...)
+	}
+
+	if t.Logs[i] == nil {
+		t.Logs[i] = &TaskLog{}
+	}
+
+	return t.Logs[i]
+}
+
+func (t *Task) GetExecLog(attempt int, i int) *ExecutorLog {
+	tl := getTaskLog(t, 0)
+
+	// Grow slice length if necessary
+	if len(tl.Logs) <= i {
+		desired := i + 1
+		tl.Logs = append(tl.Logs, make([]*ExecutorLog, desired-len(tl.Logs))...)
+	}
+
+	if tl.Logs[i] == nil {
+		tl.Logs[i] = &ExecutorLog{}
+	}
+
+	return tl.Logs[i]
+}
